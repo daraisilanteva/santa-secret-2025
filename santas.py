35d672c71c64b6453b4866ec7bfb360c1c210fac
daraisilanteva <83756078+daraisilanteva@users.noreply.github.com>
2024-12-16 23:54:00 +0300

    Update santas.py

--- santas.py
+++ santas.py
@@ -1,7 +1,4 @@
-Python 3.13.1 (v3.13.1:06714517797, Dec  3 2024, 14:00:22) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-... import random
+ ... import random
 ... import string
 ... import logging
 ... from telegram import Update, ReplyKeyboardMarkup, KeyboardButton
